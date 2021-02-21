6aa284e2fc3e57c9cfb139ad6ddf1011e87378e0
Adam Elliott <adamelliott1982@gmail.com>
2021-02-21 15:30:05 -0600

    new commit

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 // prompt('What is your secret Pass Phrase?');
 
-// let passPhrase = prompt('Type a FOUR WORD Pass Phrase:');
+let passPhrase = prompt('Type a FOUR WORD Pass Phrase:');
 
 
 // button 1
-let message = 'I like the Beatles';
+console.log('button 1');
+let message = passPhrase;
 let message2 = message.toLowerCase();
 let message3 = message.toUpperCase();
 let message4 = message.length;
 
 
 //button 2
-console.log('button2a');
+console.log('button 2');
 let button2a = message.split(' ');
 console.log(button2a);
 
-console.log('button2b');
 let button2b = message.split(' ');
 let shiftedWord = button2b.shift();
 button2b.push(shiftedWord);
 console.log(button2b);
 
-console.log('button2c');
 let button2c = message.split(' ');
 button2c.splice(2, 0, 'secret');
 console.log(button2c);
 
 
 //button 3
-console.log('');
-console.log('button3a');
+console.log('button 3');
 let button3a = message.split('');
 console.log(button3a);
 
@@ -38,3 +36,4 @@ console.log(button3b);
 
 let button3c = message.split('').sort();
 console.log(button3c);
+
